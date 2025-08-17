9a0a09a396bd5245bf9f1d5dceba9910be33f0d3
anishad6 <73513343+anishad6@users.noreply.github.com>
2025-08-17 16:41:27 +0530

    Delete scraper_backend directory

--- scraper_backend/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import  GoogleAuthView
-
-app_name = 'accounts'
-
-urlpatterns = [
-    path('api/auth/google/',  GoogleAuthView.as_view(), name='google_auth'),
-]
--- scraper_backend/scraper/urls.py
+++
@@ -1,8 +0,0 @@
-# scraper/urls.py
-from django.urls import path
-from .views import GoogleLogin
-
-urlpatterns = [
-    # ...
-    path('auth/social/google/', GoogleLogin.as_view(), name='google_login'),
-]
